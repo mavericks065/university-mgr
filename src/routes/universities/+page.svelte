bd3141e6b7a79b22f14ff431e2b2267e8274bed6
Nicolas Guignard <nicolas065@gmail.com>
2022-12-16 17:27:10 +0100

    demo app + first static page

---
+++ src/routes/universities/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+
+	$: ({universities} = data)
+</script>
+
+<svelte:head>
+	<title>Universities</title>
+	<meta name="description" content="Universities tab" />
+</svelte:head>
+
+
+{#if universities.length > 0}
+	<ul>
+		{#each universities as item}
+			<li>{item.name}</li>
+		{/each}
+	</ul>
+{/if}
---
+++ src/routes/universities/+page.ts
@@ -0,0 +1,21 @@
+import {dev} from '$app/environment';
+import type {PageLoad} from './$types';
+
+// we don't need any JS on this page, though we'll load
+// it in dev so that we get hot module replacement
+export const csr = dev;
+
+async function fetchUniversities(fetch, country: string) {
+    const result = await fetch(`http://universities.hipolabs.com/search?country=${country}`);
+    const data = await result.json();
+    return data;
+}
+
+export const load = (async ({fetch}) => {
+
+    return {
+        universities: await fetchUniversities(fetch, 'France'),
+    };
+}) satisfies PageLoad;
+
+
